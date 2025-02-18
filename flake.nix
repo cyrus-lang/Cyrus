41c670dcd95fd6f7367c59c574d983907a85933d
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-02-18 23:31:24 +0330

    improve README.md and flake.nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Cyrus language flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      };
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "cyrus";
+        version = "latest";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        nativeBuildInputs = [
+          rustToolchain
+          pkgs.gcc
+          pkgs.libgccjit
+        ];
+
+        meta = {
+          license = pkgs.lib.licenses.mit;
+          description = "Cyrus Programming Language";
+          homepage = "https://github.com/cyrus-lang/Cyrus-Lang";
+        };
+      };
+    };
+}
+
