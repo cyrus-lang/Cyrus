e249d352c9d401bc75d28d71a607ef681f42139c
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-04-11 00:09:47 +0330

    linkage problem fixed; it was a simple problem when building VisType for func.

--- crates/codegen_llvm/src/linkage.rs
+++ crates/codegen_llvm/src/linkage.rs
@@ -7,7 +7,7 @@ impl<'ctx> CodeGenLLVM<'ctx> {
     pub(crate) fn build_linkage(&self, vis_type: VisType) -> Linkage {
         match vis_type {
             VisType::Extern => Linkage::External,
-            VisType::Pub => Linkage::Internal,
+            VisType::Pub => Linkage::External,
             VisType::Internal => Linkage::Private,
             VisType::Inline => Linkage::LinkOnceODR,
         }
