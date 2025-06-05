31f45353711005602394ea4678dd6c89131b5bc7
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-06-06 01:53:35 +0330

    refactor: removed unnecessary stuff from build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,1 @@
-use std::process::Command;
-
-fn main() {
-    Command::new("gcc")
-        .args(&[
-            "-c",
-            "-fPIC",
-            "-o",
-        ])
-        .status()
-        .unwrap();
-}
+fn main() {}
