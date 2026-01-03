d3ca5ffdd1c059f68ea6b3c6afdc3f5bec130693
Max Base <MaxBaseCode@gmail.com>
2026-01-04 01:06:10 +0330

    MAX: Adding documentation/signature for all main source files

--- comments.py
+++ comments.py
@@ -1,4 +1,6 @@
-// ┌─────────────────────────────────────────────────────────────────────────┐
+import os
+
+comment_block = """// ┌─────────────────────────────────────────────────────────────────────────┐
 // │                                                                         │
 // │  Cyrus Programming Language                                             │
 // │  https://github.com/cyrus-lang/Cyrus                                    │
@@ -22,40 +24,22 @@
 // │  along with this program. If not, see <https://www.gnu.org/licenses/>.  │
 // │                                                                         │
 // └─────────────────────────────────────────────────────────────────────────┘
+"""
 
-/* 
- * Copyright (c) 2026 The Cyrus Programming Language Project
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-use cyrusc_fs_utils::read_file;
-use cyrusc_lexer::Lexer;
-use cyrusc_parser::Parser;
-use std::env;
+extensions = ('.c', '.cpp', '.rs', '.h', '.hpp')
 
-pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = args[1].clone();
-    let file_content = read_file(file_path.clone()).0;
-    let mut lexer = Lexer::new(file_content, file_path.clone());
-
-    let mut parser = Parser::new(lexer.tokenize(), file_path);
-
-    match parser.parse() {
-        Ok(result) => println!("{:#?}", result),
-        Err(errors) => {
-            parser.display_parser_errors(errors);
-        }
-    }
-}
+for root, dirs, files in os.walk('.'):
+    for filename in files:
+        if filename.endswith(extensions):
+            filepath = os.path.join(root, filename)
+            
+            with open(filepath, 'r', encoding='utf-8') as f:
+                original_content = f.read()
+            
+            if comment_block.strip() in original_content:
+                continue
+            
+            with open(filepath, 'w', encoding='utf-8') as f:
+                f.write(comment_block + '\n' + original_content)
+            
+            print(f"Updated: {filepath}")
