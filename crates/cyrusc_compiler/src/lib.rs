5f600566b19b89f3c4a78653094cfcdfacd9118e
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2026-01-04 01:33:57 +0330

    Rename to 'The Cyrus Language'

--- crates/cyrusc_compiler/src/lib.rs
+++ crates/cyrusc_compiler/src/lib.rs
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2026 The Cyrus Team
+ * Copyright (c) 2026 The Cyrus Language
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
