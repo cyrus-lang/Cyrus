e3721402f577c52648f736f50dc4cc2cdd7f458d
Max Base <MaxBaseCode@gmail.com>
2026-01-04 00:52:24 +0330

    Change comments signature style

--- crates/cyrusc_codegen_llvm/src/builder/control_flow.rs
+++ crates/cyrusc_codegen_llvm/src/builder/control_flow.rs
@@ -1,7 +1,19 @@
-// Copyright (c) 2026 Cyrus Team. All rights reserved.
-// Use of this source code is governed by a LGPLv3.0
-// a copy of which can be found in the LICENSE file.
-
+/* 
+ * Copyright (c) 2026 The Cyrus Programming Language Project
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 use crate::{
     builder::{builder::IRBuilderCtx, irreg::LocalIRValue},
     llvm::c_str::to_c_str,
