175e00f20091188458141d74d4456a223ea28edb
Max Base <MaxBaseCode@gmail.com>
2026-01-03 17:13:46 +0330

    MAX: Adding documentation/signature for all main source files

--- crates/cyrusc_ast/src/operators.rs
+++ crates/cyrusc_ast/src/operators.rs
@@ -1,3 +1,28 @@
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                                                         │
+// │  Cyrus Programming Language                                             │
+// │  https://github.com/cyrus-lang/Cyrus                                    │
+// │                                                                         │
+// │  A general-purpose, statically-typed, manually memory-managed           │
+// │  programming language designed for performance-critical applications.   │
+// │                                                                         │
+// │  Copyright (c) 2026 The Cyrus Programming Language Project              │
+// │                                                                         │
+// │  This program is free software: you can redistribute it and/or modify   │
+// │  it under the terms of the GNU General Public License as published by   │
+// │  the Free Software Foundation, either version 3 of the License, or      │
+// │  (at your option) any later version.                                    │
+// │                                                                         │
+// │  This program is distributed in the hope that it will be useful,        │
+// │  but WITHOUT ANY WARRANTY; without even the implied warranty of         │
+// │  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           │
+// │  GNU General Public License for more details.                           │
+// │                                                                         │
+// │  You should have received a copy of the GNU General Public License      │
+// │  along with this program. If not, see <https://www.gnu.org/licenses/>.  │
+// │                                                                         │
+// └─────────────────────────────────────────────────────────────────────────┘
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum InfixOperator {
     Add,
