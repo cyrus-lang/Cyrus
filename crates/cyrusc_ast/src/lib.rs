ae79dcac68e06307b88cdbcc7e42e50e35e78196
Taha Dostifam <mr.tahadostifam@gmail.com>
2026-07-30 16:46:06 +0330

    feat: changed default mutability of function parameters to const/immutable

--- crates/cyrusc_ast/src/lib.rs
+++ crates/cyrusc_ast/src/lib.rs
@@ -1269,6 +1269,12 @@ impl AssignKind {
     }
 }
 
+impl Default for Mutability {
+    fn default() -> Self {
+        Self::Const
+    }
+}
+
 impl fmt::Display for AssignKind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
